026b22d89ac3830d9e3627e996807330bd525eca
Paulo Jerônimo <paulojeronimo@gmail.com>
2025-04-07 09:55:55 -0300

    Adds docs configurations

--- docs/common/build.sh
+++ docs/common/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eou pipefail
 cd $(dirname $0)
-source ../common/README.sh
+
+common_dir=$(readlink ${BASH_SOURCE[0]})
+common_dir=${common_dir%/build.sh}
+source $common_dir/README.sh
 
 build-docs "$@"
